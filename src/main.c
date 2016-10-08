9bffd689067af4fb53679fbd335ec8c09348f9f4
Fabricio <fabricio-gf@hotmail.com>
2016-10-08 14:19:10 -0300

    Add main.c final version

--- src/main.c
+++ src/main.c
@@ -5,29 +5,19 @@
 
 #include "core/futoshiki.h"
 
-
-/*
- * Test cases 1-7
- * int main(void) {
- *     Puzzle *p = puzzle_new(stdin);
- * 
- *     printf("%d\n", _puzzle_checkValidState(p));
- * 
- *     puzzle_destroy(p);
- * 
- *     return 0;
- * }
-*/
-
 int main(void) {
-    Puzzle *p = puzzle_new(stdin);
-    clock_t t;
 
-    t = clock();
-    printf("%d\n", puzzle_solve(p));
-    fprintf(stderr, "%f\n", ((float)clock() - t) / CLOCKS_PER_SEC);
-    puzzle_display(p, stdout);
+	int i, ncases;
+	scanf("%d", &ncases);
 
-    puzzle_destroy(p);
+	for(i = 0; i < ncases; i++){
+	    Puzzle *p = puzzle_new(stdin);
+
+	    puzzle_solve(p);
+	    printf("%d\n", i);
+	    puzzle_display(p, stdout);
+
+	    puzzle_destroy(p);
+	}
     return 0;
 }
